eb17dad610ec42ab5d7d2ea11ce4525ebe1ebd7f
Nikhil gupta <gnikhil335@gmail.com>
2025-05-19 15:32:16 +0530

    Update demo suggestions to webset queries

--- components/greeting.tsx
+++ components/greeting.tsx
@@ -13,7 +13,7 @@ export const Greeting = () => {
         transition={{ delay: 0.5 }}
         className="text-5xl font-semibold"
       >
-        Hello there!
+        Welcome to the Webset demo!
       </motion.div>
       <motion.div
         initial={{ opacity: 0, y: 10 }}
@@ -22,7 +22,8 @@ export const Greeting = () => {
         transition={{ delay: 0.6 }}
         className="text-5xl text-zinc-500"
       >
-        How can I help you today?
+        Try queries like "Show a webset of top tech companies" or
+        "Create a webset of ExampleCorp employees".
       </motion.div>
     </div>
   );
